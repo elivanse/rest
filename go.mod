fbc1a83da5a4cc1d1ac1b9c1b9e117c8bcd1b97b
IvanMaximilianoPereyra <elivanse@hotmail.com>
2022-05-28 17:26:15 -0300

    done

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module rest
 
 go 1.18
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/keegancsmith/rpc v1.3.0 // indirect
+	github.com/stamblerre/gocode v1.0.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df // indirect
+)
--- main.go
+++ main.go
@@ -8,15 +8,27 @@ import (
 	"time"
 )
 
+// ejercicio extraido de la pagina 49
+// Building RESTful web sevices with go
+
 func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
+
+	/ la siguiente linea le avisa al header
+	/ que el request de la api fue exitoso
+
+	.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Category is: %v\n", vars["category"])
 	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
 }
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+
+	/ en la proxima linea le decimos que el id
+	/ es un numero del 0 al 9 y que tiene
+	/ 1 o mas digitos
+
+	.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:8081",

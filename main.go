07369c3b99362ebbd07076e996f6c866a39092e3
IvanMaximilianoPereyra <elivanse@hotmail.com>
2022-05-30 20:44:18 -0300

    probado rest

--- main.go
+++ main.go
@@ -14,21 +14,21 @@ import (
 func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
-	/ la siguiente linea le avisa al header
-	/ que el request de la api fue exitoso
+	// la siguiente linea le avisa al header
+	// que el request de la api fue exitoso
 
-	.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Category is: %v\n", vars["category"])
 	fmt.Fprintf(w, "ID is: %v\n", vars["id"])
 }
 func main() {
 	r := mux.NewRouter()
 
-	/ en la proxima linea le decimos que el id
-	/ es un numero del 0 al 9 y que tiene
-	/ 1 o mas digitos
+	// en la proxima linea le decimos que el id
+	// es un numero del 0 al 9 y que tiene
+	// 1 o mas digitos
 
-	.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
+	r.HandleFunc("/articles/{category}/{id:[0-9]+}", ArticleHandler)
 	srv := &http.Server{
 		Handler:      r,
 		Addr:         "127.0.0.1:8081",
